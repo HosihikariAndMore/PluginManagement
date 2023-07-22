cbcdff76e602eb14cd934b98e1280db4d54dd95d
LazuliKao <lazulikao@nbt.edu.cn>
2023-07-22 18:59:13 +0800

    fix: multi entry-point

--- src/PluginManager.cs
+++ src/PluginManager.cs
@@ -6,7 +6,7 @@ namespace Loader;
 
 internal static class PluginManager
 {
-    public static List<(AssemblyLoadContext ctx, IPlugin plugin)> PluginContexts { get; }
+    public static List<(AssemblyLoadContext ctx, List<IPlugin> plugin)> PluginContexts { get; }
 
     static PluginManager()
     {
@@ -18,13 +18,15 @@ internal static class PluginManager
         AssemblyLoadContext loadContext = new(path);
         Assembly assembly = loadContext.LoadFromAssemblyPath(path);
         var success = false;
+        var plugins = new List<IPlugin>();
         foreach (var entryPoint in assembly.GetCustomAttributes<EntryPointAttributeBase>())
         {
             var plugin = entryPoint.CreateInstance();
             plugin.Initialize();
-            PluginContexts.Add((loadContext, plugin));
             success = true;
+            plugins.Add(plugin);
         }
+        PluginContexts.Add((loadContext, plugins));
         return success;
     }
 }
